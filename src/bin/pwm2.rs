0d48326217b085a952b3c28bba3172166bdadf33
Ethan <edenis1705@gmail.com>
2025-03-21 11:11:51 +1100

    Added a new item to checklist

--- src/bin/pwm2.rs
+++ src/bin/pwm2.rs
@@ -21,6 +21,7 @@
 // [_] view all entry names
 // [_] remove an entry
 // [_] add a way to import data from old versions
+// [_] add a way to generate random but memerable passwords
 
 use pwm2_rust::{Entry, SMsg};
 
