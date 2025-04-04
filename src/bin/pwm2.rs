5572a0bbe7a6a9b171eae4d2ff2981ba9f054d6a
Ethan <edenis1705@gmail.com>
2025-04-04 12:52:59 +1100

    Removed unneeded checklist items

--- src/bin/pwm2.rs
+++ src/bin/pwm2.rs
@@ -19,9 +19,7 @@
 // [x] viewable backups
 // [x] restore backup
 // [x] remove an entry
-// [_] add a way to import data from old versions
 // [_] add a way to generate random but memerable passwords
-// [_] add a new entry with a random password
 
 use pwm2_rust::{
     *,
